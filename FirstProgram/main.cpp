f5e769c245685ddbaee258541f98b1dc50df8e20
Robert Lowe <robertmlowe@rmlowe.com>
2021-04-05 00:59:01 +0800

    Building our first program

--- FirstProgram/main.cpp
+++ FirstProgram/main.cpp
@@ -3,13 +3,8 @@
 int main()
 {
     int favorite_number;
-    
     std::cout << "Enter your favorite number between 1 and 100: ";
-    
     std::cin >> favorite_number;
-    
     std::cout << "Amazing!! That's my favorite number too!" << std::endl;
-    
-    
     return 0;
 }
